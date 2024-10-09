31dce586c9c203a483c2d040c15a1a516c2c6a2f
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-10-09 23:43:22 +0700

    Update product-create.tsx

--- src/pages/dashboards/products/product-create.tsx
+++ src/pages/dashboards/products/product-create.tsx
@@ -1,4 +1,6 @@
 import { useParams } from 'react-router-dom';
+
+
 import WrapperCard from '../../../components/wrappers/wrapper-card';
 import RegularInput from '../../../components/general/regular-input';
 
