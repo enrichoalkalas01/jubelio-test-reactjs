fb5e97000fb31f7825bf56432541334e8a193770
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-10-11 19:58:13 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 500, // Adjust the limit as needed
-  }
 })
